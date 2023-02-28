b09ada54227b6cbec3401e807308926752ab1b5d
cobyphil <122822733+cobyphil@users.noreply.github.com>
2023-02-28 13:45:22 +0000

    Create 2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _strlen - check string lenth
+ * 
+ * @length: length of string
+ * Return: int
+ */
+
+int _strlen(char *s)
+{
+int length = 0;
+length = strlen(*s);
+  return(length);
+}
