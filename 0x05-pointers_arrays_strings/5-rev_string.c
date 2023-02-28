539f1c18edd67efccc75aef4dad038371e302f48
cobyphil <122822733+cobyphil@users.noreply.github.com>
2023-02-28 18:31:04 +0000

    Update 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -10,13 +10,14 @@
 
 void rev_string(char *s)
 {
-    int len = strlen(s);
-    char temp;
-    int i;
-    for (i = 0; i < len / 2; i++)
-    {
-        temp = s[i];
-        s[i] = s[len - i - 1];
-        s[len - i - 1] = temp;
-    }
+int len = strlen(s);
+char temp;
+int i;
+
+for (i = 0; i < len / 2; i++)
+{
+temp = s[i];
+s[i] = s[len - i - 1];
+s[len - i - 1] = temp;
+}
 }
