f66240afd509f3f195f54b9d82d77e0e5fe25870
cobyphil <122822733+cobyphil@users.noreply.github.com>
2023-02-28 20:30:28 +0000

    Update 6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -3,8 +3,8 @@
 void puts2(char *str)
  {
     int i;
-    for (i = 0; s[i] != '\0'; i += 2) {
-        printf("%c", s[i]);
+    for (i = 0; str[i] != '\0'; i += 2) {
+        printf("%c", str[i]);
     }
     printf("\n");
 }
